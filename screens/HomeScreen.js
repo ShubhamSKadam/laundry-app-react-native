2af2c07cc9e7392811a8e81e7b97ab211cb59035
Shubham.kadam <shubham.kadam@MacBook-Air.local>
2023-07-24 15:03:09 +0530

    Search UI with icon added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,16 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  Image,
+  TextInput,
+} from "react-native";
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
@@ -43,7 +53,7 @@ const HomeScreen = () => {
   const getCurrentLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
 
-    if (status !== 'granted') {
+    if (status !== "granted") {
       Alert.alert(
         "Permission Denied",
         "Allow the app to use the location services",
@@ -65,7 +75,7 @@ const HomeScreen = () => {
     }
 
     const { coords } = await Location.getCurrentPositionAsync();
-    console.log(coords);
+    // console.log(coords);
 
     if (coords) {
       const { latitude, longitude } = coords;
@@ -75,7 +85,7 @@ const HomeScreen = () => {
         longitude,
       });
 
-      console.log(response);
+      //   console.log(response);
 
       for (let item of response) {
         let address = `${item.name} ${item.city} ${item.postalCode}`;
@@ -86,7 +96,46 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
+      {/* Location and Profile */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          padding: 10,
+        }}
+      >
+        <Ionicons name="location" size={24} color="#fd5c63" />
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+          <Text>{displayCurrentAddress}</Text>
+        </View>
+
+        <Pressable style={{ marginLeft: "auto", marginRight: 7 }}>
+          <Image
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+            source={{
+              uri: "https://pbs.twimg.com/profile_images/1519851219313135616/aGV94IOE_400x400.jpg",
+            }}
+          />
+        </Pressable>
+      </View>
+
+      {/* Search Bar */}
+      <View
+        style={{
+          flexDirection: "row",
+          padding: 10,
+          margin: 10,
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderWidth: 0.8,
+          borderColor: "#c0c0c0",
+          borderRadius: 7,
+        }}
+      >
+        <TextInput placeholder="Search for Items for More" />
+        <Feather name="search" size={24} color="#fd5c63" />
+      </View>
     </SafeAreaView>
   );
 };
