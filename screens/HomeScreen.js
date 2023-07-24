b1149255ec38fc53d48da90b92e0dc4782bd3bbd
Shubham.kadam <shubham.kadam@MacBook-Air.local>
2023-07-24 12:22:19 +0530

    Done implementing and getting the location service

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -40,6 +40,50 @@ const HomeScreen = () => {
     }
   };
 
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert(
+        "Permission Denied",
+        "Allow the app to use the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          {
+            text: "Okay",
+            onPress: () => console.log("Okay Pressed"),
+          },
+        ],
+        {
+          cancelable: false,
+        }
+      );
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+    console.log(coords);
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      console.log(response);
+
+      for (let item of response) {
+        let address = `${item.name} ${item.city} ${item.postalCode}`;
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
