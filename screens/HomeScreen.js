f034f0c05fe2501d698af356cf7545ab5692e8c1
Shubham.kadam <shubham.kadam@MacBook-Air.local>
2023-07-27 11:49:53 +0530

    Finished Implementing the UI screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import {
   Pressable,
   Image,
   TextInput,
+  ScrollView,
 } from "react-native";
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
@@ -13,6 +14,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import Caraousel from "../components/Caraousel";
 import Services from "../components/Services";
+import DressItem from "../components/DressItem";
 
 const HomeScreen = () => {
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
@@ -96,8 +98,61 @@ const HomeScreen = () => {
     }
   };
 
+  // products data
+  const services = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "T-shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "dresses",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "jeans",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/9431/9431166.png",
+      name: "Sweater",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/3345/3345397.png",
+      name: "shorts",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/293/293241.png",
+      name: "Sleeveless",
+      quantity: 0,
+      price: 10,
+    },
+  ];
+
   return (
-    <SafeAreaView style={{ backgroundColor: "#F0F0F0", flex: 1 }}>
+    <ScrollView style={{ backgroundColor: "#F0F0F0", flex: 1, marginTop:50 }}>
       {/* Location and Profile */}
       <View
         style={{
@@ -144,7 +199,12 @@ const HomeScreen = () => {
 
       {/* Services */}
       <Services />
-    </SafeAreaView>
+
+      {/* Render all the products */}
+      {services.map((item,index)=>(
+        <DressItem item={item} key={index}/>
+      ))}
+    </ScrollView>
   );
 };
 
